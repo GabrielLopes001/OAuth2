f6feccba0714caeb0d59ee95fb0587393e14c5a6
Gabriel Lopes <gabriejob1@outlook.com>
2024-10-22 21:04:13 -0300

    Initial Commit

---
+++ OAuth-exemple/OAuth_exempleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OAuth_exempleApp.swift
+//  OAuth-exemple
+//
+//  Created by Gabriel Lopes on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct OAuth_exempleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
